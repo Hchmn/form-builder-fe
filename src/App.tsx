fb5fd633e57f5dbe92e6f2aee9fd7915454595cc
Nash Gandamato <nashgandamato@Nashs-MacBook-Pro.local>
2025-03-01 19:53:18 +0800

    [ADD] - add baseUrl constant and set routes path with baseurl.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import FormEditor from './modules/form-builder/Editor';
-
-const DefaultPage = () => {
-  return (
-    <div>
-      <h1>This is just a sample page</h1>
-    </div>
-  );
-};
+import { baseUrl } from './constants';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<DefaultPage />} />
-          <Route path="/editor" element={<FormEditor />} />
+          <Route path={`${baseUrl}`} element={<FormEditor />} />
         </Routes>
       </Router>
     </div>
