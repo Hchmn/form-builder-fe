432c95ba07c67b17a791914bf901fd40c20c3217
Nash <nashgandamato@gmail.com>
2025-03-09 17:31:00 +0800

    [DONE] - created custom workspacetab component.

--- src/modules/form-builder/WorkspaceManager.tsx
+++ src/modules/form-builder/WorkspaceManager.tsx
@@ -1,26 +1,17 @@
 import { useWorkbench } from '@designable/react';
 import { Observer } from '@formily/react';
-import { Button } from 'antd';
 import { Workspace } from './Workspace';
 import { Field } from './components';
 import { Form } from './components/Form/component';
 import { Input } from './components/Input/component';
+import { WorkspaceTab } from '../../components/tabs/WorkspaceTab';
 
 export const WorkspaceManager = () => {
   const workbench = useWorkbench();
 
   const workspaceTabs = () =>
     workbench.mapWorkspace((workspace) => (
-      <Button
-        onClick={() => {
-          workbench.switchWorkspace(workspace.id);
-          workbench.setActiveWorkspace(workspace);
-        }}
-        disabled={workbench.currentWorkspace.id === workspace.id}
-        key={'workspace-tab-' + workspace.id}
-      >
-        {workspace.title}
-      </Button>
+      <WorkspaceTab workspace={workspace}/>
     ));
 
   return (
