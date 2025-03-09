126f8d5b60e114250db62150bfd95844223a9bd4
Nash <nashgandamato@gmail.com>
2025-03-09 18:21:51 +0800

    [EDIT] - WorkpsaceTab changed to Generic ClosableTab.

--- src/modules/form-builder/WorkspaceManager.tsx
+++ src/modules/form-builder/WorkspaceManager.tsx
@@ -4,14 +4,33 @@ import { Workspace } from './Workspace';
 import { Field } from './components';
 import { Form } from './components/Form/component';
 import { Input } from './components/Input/component';
-import { WorkspaceTab } from '../../components/tabs/WorkspaceTab';
+import { CloseableTag } from '../../components/tabs/ClosableTag';
+import { CloseOutlined } from '@ant-design/icons';
 
 export const WorkspaceManager = () => {
   const workbench = useWorkbench();
 
   const workspaceTabs = () =>
     workbench.mapWorkspace((workspace) => (
-      <WorkspaceTab workspace={workspace}/>
+      <CloseableTag
+        label={workspace.title}
+        color={
+          workbench.currentWorkspace.id === workspace.id ? 'blue' : 'default'
+        }
+        closable={true}
+        onClick={() => {
+          workbench.switchWorkspace(workspace.id);
+          workbench.setActiveWorkspace(workspace);
+        }}
+        onClose={() => {
+          workbench.removeWorkspace(workspace.id);
+        }}
+        closeIcon={
+          <CloseOutlined
+            style={{ marginLeft: 8, cursor: 'pointer', color: '#999' }}
+          />
+        }
+      />
     ));
 
   return (
